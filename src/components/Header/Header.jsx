97e8ec0434de655c6cbf5030dc7588630e9249d4
marta szuran <marta.szuran@dataart.com>
2022-12-16 11:40:29 +0100

    review fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
-import {} from './Header.css'
-import { Button, TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material';
 import ToDoImage from '../../images/to-do-list.png';
+import './Header.css';
 
 export default function Header() {
     return (
-        <>
-            <div className='header'>
-                <img src={ToDoImage} alt='ToDoList' className="image"/>
-                <Button className='addButton' variant="outlined" color="success" size="large">Add new task</Button>
-                <TextField className='searchTextField' variant='outlined' label='Search' color="success" size="small" ></TextField>
-            </div>
-        </>
+        <div className="header">
+            <img src={ToDoImage} alt="ToDoList" className="logoImage"/>
+            <Button variant="outlined" color="success" size="large">Add new task</Button>
+            <TextField variant="outlined" label="Search" color="success" size="small" />
+        </div>
     )
 }
