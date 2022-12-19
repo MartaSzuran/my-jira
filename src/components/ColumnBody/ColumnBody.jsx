a04fd9607a8f1d669579203c4493209f85d3ef52
marta szuran <marta.szuran@dataart.com>
2022-12-19 14:32:13 +0100

    adding dnd react librabry and implement drag for card

--- src/components/ColumnBody/ColumnBody.jsx
+++ src/components/ColumnBody/ColumnBody.jsx
@@ -1,14 +1,24 @@
 import { Card, Typography } from '@mui/material';
+import { useDrag } from 'react-dnd';
+import { ItemTypes } from '../../constants/itemTypes';
 import './ColumnBody.css';
 
 export default function ColumnBody({ todoTasksList }) {
+    const [, drag] = useDrag(() => ({
+        type: ItemTypes.CARD,
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+
     const tasksList = todoTasksList.map(({title, author, description}) => 
-        <Card key={title} variant="outlined" className="cardComponent">
+        <Card ref={drag} key={title} variant="outlined" className="cardComponent" >
             <Typography variant="overline">{title}</Typography>
             <Typography variant="subtitle2">{author}</Typography>
             <Typography variant="body2" >{description}</Typography>
         </Card>
     )
+
 
     return (
         <div className="columnBody">
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,8 @@
 import Header from '../Header/Header.jsx';
 import Columns from '../Columns/Columns.jsx'
 import tasksList from '../../data/tasks.js';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 
 export default function MainPage() {
@@ -9,7 +11,9 @@ export default function MainPage() {
     return (
         <div>
             <Header />
-            <Columns tasks={TASKS}/>
+            <DndProvider backend={HTML5Backend}>
+                <Columns tasks={TASKS}/>
+            </DndProvider>
         </div>
     )
 }
