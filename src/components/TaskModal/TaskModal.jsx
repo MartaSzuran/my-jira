bf30afd62c0f47c7b3b90e4de9cbc62303d7e14f
marta szuran <marta.szuran@dataart.com>
2022-12-27 09:55:28 +0100

    adding edition with some issues

--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -1,5 +1,8 @@
-import { Box, Button, Select, FormControl, InputLabel, Typography, Modal, MenuItem } from "@mui/material";
+import { useState } from 'react';
+import { Box, Button, Select, FormControl, InputLabel, Typography, Modal, MenuItem, OutlinedInput, ClickAwayListener } from "@mui/material";
 import Close from '@mui/icons-material/Close';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 import './TaskModal.css';
 
 export default function OpeningTaskModal({ 
@@ -12,7 +15,9 @@ export default function OpeningTaskModal({
     }) {
 
     const {title, description, id, type} = task;
-    
+    const [enableTitleEdition, setEnableTitleEdition] = useState(false);
+    const [newTitle, setNewTitle] = useState(description);
+
     const chooseTaskType = ({target: {value}}) => {
         const currentTasks = taskList.filter((task) => {
             if (task.id === id) {
@@ -24,6 +29,58 @@ export default function OpeningTaskModal({
         setTaskList(currentTasks);
     };
 
+    const handleTitleChange = ({target: { value }}) => {
+        setNewTitle(value);
+        setEnableTitleEdition(false);
+    }
+
+    const saveNewTitle = () => {
+        const currentTasks = taskList.filter((task) => {
+            if (task.id === id) {
+                task.title = newTitle;
+                return task;
+            } 
+            return task;
+        });
+        setTaskList(currentTasks);
+    }
+
+    const cancelTitleChanges = () => {
+        setNewTitle('');
+        setEnableTitleEdition(false);
+    }
+
+    const Title = () => {
+        if (enableTitleEdition) {
+            return (
+                <FormControl focused={true}>
+                    <InputLabel htmlFor="titleInput">Title</InputLabel>
+                    <ClickAwayListener onClickAway={cancelTitleChanges}>
+                        <OutlinedInput
+                            placeholder={title}
+                            id="titleInput"
+                            label="Title"
+                            value={newTitle}
+                            onChange={handleTitleChange}
+                        />
+                    </ClickAwayListener>
+                    <Box className="buttonsSaveCancel">
+                        <Button disabled={newTitle ? false : true}>
+                            <CheckIcon onClick={saveNewTitle} />
+                        </Button>
+                        <Button>
+                            <ClearIcon onClick={cancelTitleChanges} /> 
+                        </Button>
+                    </Box>
+                </FormControl>
+            )
+        }
+        return (
+            <Typography variant="h2" className="modalTitle" >
+                {title}
+            </Typography>
+        )
+    }
     return (
         <Modal
             open={open}
@@ -39,9 +96,9 @@ export default function OpeningTaskModal({
                 </Button>
             </Box>
             
-            <Typography variant="h2" className="modalTitle">
-                {title}
-            </Typography>
+            <Box onClick={() => setEnableTitleEdition(true)} className="modalInputTitle" >
+                <Title />
+            </Box>
 
             <Box className="tasksTypesSelectStyle">
                 <FormControl>
@@ -52,6 +109,7 @@ export default function OpeningTaskModal({
                         value={type}
                         label="Set task status"
                         onChange={chooseTaskType}
+                        className="selectStyle"
                     >
                         {optionsTasksTypes.map((option) => (
                             <MenuItem key={option} value={option}>
